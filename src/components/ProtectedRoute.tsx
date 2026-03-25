78f6cfe29068b48eed33ddfaf95212957e3ae804
kishanMFS <kishanp@mindfiresolutions.com>
2026-03-25 11:18:14 +0530

    refactor:  updated validation to validate file size

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,35 +1,33 @@
-import { useEffect } from 'react';
-import { useLocation, Navigate, useNavigate } from 'react-router-dom';
-import { UserAuth } from '../context/authenticationContext.tsx';
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/authenticationContext.tsx";
 import { useErrorContext } from "../context/ErrorContext.tsx";
 
 type childrenType = {
-    children : React.ReactNode
+  children: React.ReactNode;
+};
+
+function ProtectedRoute({ children }: childrenType) {
+  const { showErrorMessage } = useErrorContext();
+  const { logout } = UserAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    try {
+      const token = localStorage.getItem("jwtToken");
+      const isLoginPage = location.pathname === "/login";
+      if (token) {
+        if (isLoginPage) navigate("projects", { replace: true });
+      } else {
+        logout();
+      }
+    } catch (e: React.ReactEventHandler) {
+      showErrorMessage(e.message);
+    }
+  }, [location.pathname, logout, navigate, showErrorMessage]);
+
+  return children;
 }
 
-function ProtectedRoute({ children}: childrenType){
-    const { showErrorMessage } = useErrorContext();
-    const { logout } = UserAuth();
-    const navigate = useNavigate()
-    const location = useLocation()
-
-    useEffect(()=>{
-        try{
-            const token = localStorage.getItem("jwtToken")
-            const isLoginPage = location.pathname === '/login'
-            if(token){
-                if(isLoginPage) navigate('projects', {replace: true})
-            }
-            else{
-                logout()           
-            }
-        }
-        catch(e){
-            showErrorMessage(e.message);
-        }
-    }, [location.pathname])
-    
-    return children
-}
-
-export default ProtectedRoute
+export default ProtectedRoute;
--- src/pages/ProjectFiles.tsx
+++ src/pages/ProjectFiles.tsx
@@ -1,180 +1,211 @@
+import React, { useEffect, useState, useReducer } from "react";
+import { useParams } from "react-router-dom";
 
-import { useEffect, useState, useReducer } from 'react';
-import { useParams } from 'react-router-dom';
-
-import type { projectType } from '../reducers/projectReducers';
-import { projectReducer, getProjects } from '../reducers/projectReducers';
+import type { projectType } from "../reducers/projectReducers";
+import { projectReducer, getProjects } from "../reducers/projectReducers";
+import { useErrorContext } from "../context/ErrorContext.tsx";
 
 interface fileType {
-    name: string;
-    size: number;
-    type: string;
-    fileData: string;
-    uploadedDate: string;
+  name: string;
+  size: number;
+  type: string;
+  fileData: string;
+  uploadedDate: string;
 }
 
 function ProjectFiles() {
-    const [files, setFiles ] = useState<File[]>([]);
-    
-    const [ projects, dispatchProjectReducer ] = useReducer(projectReducer, [], getProjects)
+  const [files, setFiles] = useState<File[]>([]);
+  const FILE_MAX_SIZE = 1024 * 10; // 10kb
+  const { showErrorMessage } = useErrorContext();
 
-    const { projectId } = useParams<string>()
-    const [ hasFiles, setHasFiles ] = useState(false);
-    const [ message, setMessage ] = useState('');
-    const [ btnDisabled, setBtnDisabled ] = useState(true);
-    const [ currentProjectFiles, setCurrentProjectFiles ] = useState<fileType>([]);
+  const [projects, dispatchProjectReducer] = useReducer(
+    projectReducer,
+    [],
+    getProjects,
+  );
 
-    function handleFileChange (e:any) {
-        if(!e.target.files || e.target.files.length === 0) return
-        setFiles(Array.from(e.target.files))
-        setHasFiles(true);
-        setBtnDisabled(false);
+  const { projectId } = useParams<string>();
+  const [hasFiles, setHasFiles] = useState(false);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const foundProject = projects.find(
+    (project: projectType) => project.id === projectId,
+  );
+  const [currentProjectFiles, setCurrentProjectFiles] = useState<fileType>(
+    foundProject?.projectFiles || [],
+  );
+
+  function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files || e.target.files.length === 0) return;
+    setFiles(Array.from(e.target.files));
+    setHasFiles(true);
+    setBtnDisabled(false);
+  }
+
+  const convertToBase64 = (file: File) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("projects", JSON.stringify(projects));
+  }, [projects]);
+
+  async function handleFileUpload() {
+    const newFiles = await Promise.all(
+      files
+        .filter((file) => file.size / 1000 <= FILE_MAX_SIZE)
+        .map(async (file) => ({
+          name: file.name,
+          size: file.size,
+          type: file.type,
+          fileData: await convertToBase64(file),
+          uploadedDate: new Date().toISOString().split("T")[0],
+        })),
+    );
+
+    const existingFiles = foundProject.projectFiles || [];
+    const updatedProject = {
+      ...foundProject,
+      projectFiles: [...existingFiles, ...newFiles],
+    };
+    setCurrentProjectFiles([...existingFiles, ...newFiles]);
+
+    dispatchProjectReducer({ type: "UPDATE_PROJECT", payload: updatedProject });
+
+    showErrorMessage("Files uploaded successfully!");
+    setTimeout(() => {
+      showErrorMessage("");
+    }, 1000);
+    setFiles([]);
+    setHasFiles(false);
+    setBtnDisabled(true);
+  }
+
+  function handleFileDelete(index: number) {
+    const updatedFiles = currentProjectFiles.filter(
+      (file: fileType, i: number) => i !== index,
+    );
+    setCurrentProjectFiles(updatedFiles);
+    // const foundProject = projects.find((project: projectType) => project.id === projectId);
+    const updatedProject = {
+      ...foundProject,
+      projectFiles: updatedFiles,
+    };
+    dispatchProjectReducer({ type: "UPDATE_PROJECT", payload: updatedProject });
+  }
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    if (!e.dataTransfer.files) return;
+    setHasFiles(true);
+    setBtnDisabled(false);
+    setFiles(Array.from(e.dataTransfer.files));
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (!foundProject) {
+      showErrorMessage("No project found. Please create a project first.");
+    } else {
+      showErrorMessage("");
     }
-    
-    const convertToBase64 = ((file: File) =>{
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader()
-            reader.onload = () => resolve(reader.result)
-            reader.onerror = reject
-            reader.readAsDataURL(file)
-        })
-    })
+  }, [foundProject, showErrorMessage]);
 
-    useEffect(() => {
-        localStorage.setItem('projects', JSON.stringify(projects));
-    }, [projects]);
+  return (
+    <div>
+      {foundProject ? (
+        <div className="project-files-page">
+          <h1>Project Files Page</h1>
+          <p>Welcome to the project files page!</p>
 
-    async function handleFileUpload(e: any) {
-        const foundProject = projects.find((project: projectType) => project.id === projectId);
-        const newFiles = []
-        for (const file of files) {
-            const base64File = await convertToBase64(file)
-            newFiles.push({
-                name: file.name,
-                size:file.size,
-                type: file.type,
-                fileData: base64File,
-                uploadedDate: new Date().toISOString().split('T')[0]
-            })
-        }
-        const existingFiles = foundProject.projectFiles || [];
-        const updatedProject = {
-            ...foundProject,
-            projectFiles: [...existingFiles, ...newFiles]
-        }
-        setCurrentProjectFiles([...existingFiles, ...newFiles]);
-        
-        const updatedProjects:projectType = projects.map((project: projectType) => 
-            project.id === projectId ? updatedProject : project
-        );
-        
-        dispatchProjectReducer({ type: "UPDATE_PROJECT", payload: updatedProject })
-        // localStorage.setItem('projects', JSON.stringify(updatedProjects));
-
-        setMessage('Files uploaded successfully!');
-        setTimeout( () =>{
-            setMessage('');
-        }, 1000)
-        setFiles([]);
-        setHasFiles(false);
-        setBtnDisabled(true);
-    }
-
-    function handleFileDelete(index: number) {
-        const updatedFiles = currentProjectFiles.filter((file: fileType, i: number) => i !== index);
-        setCurrentProjectFiles(updatedFiles);
-        const foundProject = projects.find((project: projectType) => project.id === projectId);
-        const updatedProject = {
-            ...foundProject,
-            projectFiles: updatedFiles
-        }
-        dispatchProjectReducer({ type: "UPDATE_PROJECT", payload: updatedProject })
-    }
-
-    const handleDrop = (e:any) => {
-        e.preventDefault();
-        if(!e.dataTransfer.files) return
-        setHasFiles(true);
-        setBtnDisabled(false)
-        setFiles(Array.from(e.dataTransfer.files))
-    }
-
-    const handleDragOver = (e:any) => {
-        e.preventDefault();
-    }
-
-    useEffect (() =>{
-        const foundProject = projects.find((project: projectType) => project.id === projectId);
-        if (projects.length === 0 || !foundProject) {
-            setMessage('No project found. Please create a project first.');
-            return;
-        }        
-        setCurrentProjectFiles(foundProject.projectFiles || []);
-    }, [])
-
-    return (
-        <div>
-
-            {message.length ? <div className="message">{message}</div> : 
-            <div className="project-files-page">
-                <h1>Project Files Page</h1>
-                <p>Welcome to the project files page!</p>
-
-                <div className='upload-area'>
-                    <div className="file-upload-section" onDrop={handleDrop} onDragOver={handleDragOver}>
-                        <label htmlFor="ProjectFile" className="file-upload-btn">
-                            <div className="drag-section">
-                                {
-                                    files.map((file: File, index: number) => (
-                                        <div className="drag-files-field" key={index}>{file.name} )</div>
-                                    ))                                    
-                                }
-                                <div className="drag-area">
-                                    <p className={hasFiles ? "hide-me" : ""}>Drag and drop files here</p>
-                                </div>    
-                                <input type="file" id="ProjectFile" name="ProjectFile" multiple onChange={handleFileChange} />
-                            </div>
-                        </label>
+          <div className="upload-area">
+            <div
+              className="file-upload-section"
+              onDrop={handleDrop}
+              onDragOver={handleDragOver}
+            >
+              <label htmlFor="ProjectFile" className="file-upload-btn">
+                <div className="drag-section">
+                  {files.map((file: File, index: number) => (
+                    <div className="drag-files-field" key={index}>
+                      {file.name} )
                     </div>
-                    <div>
-                        <button className='btn' type="button" disabled={btnDisabled} onClick={handleFileUpload}>Upload</button>
-                    </div>
+                  ))}
+                  <div className="drag-area">
+                    <p className={hasFiles ? "hide-me" : ""}>
+                      Drag and drop files here
+                    </p>
+                  </div>
+                  <input
+                    type="file"
+                    id="ProjectFile"
+                    name="ProjectFile"
+                    multiple
+                    onChange={handleFileChange}
+                  />
                 </div>
-                <div className="files-container">
-                    <div className="preview-section files-section">
-                        <h2>Files Preview</h2>
-                        {
-                            files.map((file: File, index: number) => (
-                                <div className="files-field" key={index}>
-                                    <div>{file.name}</div>
-                                    <div className="file-size">size : {file.size} bytes</div>
-                                </div>
-                            ))
-                        }
-
-                    </div>
-                    <div className="uploaded-files-section files-section">
-                        <h2>Uploaded Files</h2>
-                        <div className="files-content">
-                            {
-                                currentProjectFiles.map((file: fileType, index: number) => (
-                                    <div className="files-field" key={index}>
-                                        <div className="file-delete-btn">
-                                            <span className="file-delete" onClick={() => handleFileDelete(index)}>x</span>
-                                        </div>
-                                        <div>{file.name}</div>
-                                        <div className="file-size">size : {file.size} bytes | Uploaded Date: {file.uploadedDate}</div>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-                
+              </label>
             </div>
-            }
+            <div>
+              <button
+                className="btn"
+                type="button"
+                disabled={btnDisabled}
+                onClick={handleFileUpload}
+              >
+                Upload
+              </button>
+            </div>
+          </div>
+          <div className="files-container">
+            <div className="preview-section files-section">
+              <h2>Files Preview</h2>
+              {files.map((file: File, index: number) => (
+                <div className="files-field" key={index}>
+                  <div>{file.name}</div>
+                  <div className="file-size">size : {file.size / 1000} Kb</div>
+                  <div className="file-error">
+                    {file.size > FILE_MAX_SIZE ? "File size too big" : ""}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="uploaded-files-section files-section">
+              <h2>Uploaded Files</h2>
+              <div className="files-content">
+                {currentProjectFiles.map((file: fileType, index: number) => (
+                  <div className="files-field" key={index}>
+                    <div className="file-delete-btn">
+                      <span
+                        className="file-delete"
+                        onClick={() => handleFileDelete(index)}
+                      >
+                        x
+                      </span>
+                    </div>
+                    <div>{file.name}</div>
+                    <div className="file-size">
+                      size : {file.size / 1000} Kb | Uploaded Date:{" "}
+                      {file.uploadedDate}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
-export default ProjectFiles
+export default ProjectFiles;
